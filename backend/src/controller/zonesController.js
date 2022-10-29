94f9af4ecedad630be0ccd9ea118e9b60b9b1fbe
Gustavo Riffel <5riffg16@solent.ac.uk>
2022-10-29 16:56:51 +0100

    Completes CRUD implementation

--- backend/src/controller/zonesController.js
+++ backend/src/controller/zonesController.js
@@ -23,12 +23,66 @@ exports.create = async (req, res) => {
   return res.status(201).json(newRecord);
 };
 
-exports.findAllCountries = async(req, res) => {
+exports.findAllCountries = async (req, res) => {
   let countries;
   try {
-    countries = await prisma.zones.findMany()
+    countries = await prisma.zones.findMany({
+      select: {
+        country: true,
+      },
+    });
   } catch (error) {
     res.status(404).send({ message: error });
   }
   return res.status(200).json(countries);
-}
+};
+
+exports.findById = async (req, res) => {
+  let record;
+  try {
+    record = await prisma.zones.findUniqueOrThrow({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+  } catch (error) {
+    res.status(400).send({ message: error });
+  }
+  return res.status(201).json(record);
+};
+
+exports.update = async (req, res) => {
+  let updatedRecord;
+  try {
+    updatedRecord = await prisma.zones.update({
+      where: {
+        id: Number(req.params.id),
+      },
+      data: {
+        country: req.body.country,
+        land_zone: req.body.land_zone,
+        mobile_zone: req.body.mobile_zone,
+        land_tariff: req.body.land_tariff,
+        mobile_tariff: req.body.mobile_tariff,
+        mobile: req.body.mobile,
+        landline: req.body.landline,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({ message: error });
+  }
+  return res.status(200).json(updatedRecord);
+};
+
+exports.delete = async (req, res) => {
+  try {
+    await prisma.zones.delete({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+  } catch (error) {
+    res.status(400).send({ message: error });
+  }
+  res.status(204).send();
+};
--- backend/src/router/zones.js
+++ backend/src/router/zones.js
@@ -3,7 +3,13 @@ const zonesRouter = express.Router();
 const zonesController = require("../controller/zonesController");
 
 
-zonesRouter.post("/create", zonesController.create);
 zonesRouter.get("/countries", zonesController.findAllCountries);
+zonesRouter.get("/countries/:id", zonesController.findById);
+
+zonesRouter.post("/create", zonesController.create);
+
+zonesRouter.put("/countries/update/:id", zonesController.update);
+
+zonesRouter.delete("/countries/delete/:id", zonesController.delete);
 
 module.exports = zonesRouter;
